4dd13dcc7ae4b0d134cd10ed9c6f9017679e1c23
JoanIgnasiMarti <44504641+JoanIgnasiMarti@users.noreply.github.com>
2021-08-25 13:25:49 +0200

    Update convo.js

--- p5js_code/convo.js
+++ p5js_code/convo.js
@@ -8,6 +8,18 @@ function setup(){
   createCanvas(wid, hei);
 };
 function draw(){
+  if (keyCode == 37 && curX>0){
+    curX--;
+  }
+  else if(keyCode == 38 && curY>0){
+    curY--;
+  }
+  else if(keyCode == 39 && curX < int(displayWidth/caja)){
+    curX++;
+  }
+  else if(keyCode == 40 && curY < int(displayHeight/caja)){
+    curY++;
+  }
   background(0);
   for (let i=0;i<int(displayWidth/caja);i++){
     for (let j=0;j<int(displayHeight/caja);j++){
