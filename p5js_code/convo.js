d8ff13395c73fe0eb11d24fc6fea7ce747bf12ed
JoanIgnasiMarti <44504641+JoanIgnasiMarti@users.noreply.github.com>
2021-08-25 12:51:50 +0200

    Update convo.js

--- p5js_code/convo.js
+++ p5js_code/convo.js
@@ -1,10 +1,11 @@
 log("testing");
 let caja = 32;
+let factor = 0.6;
 function setup(){
   log("should work");
-  let wid = int(windowWidth*0.8/caja)*caja;
-  let hei = int(windowHeight*0.8/caja)*caja;
-  createCanvas(windowWidth*0.8, windowHeight*0.8);
+  let wid = int(windowWidth*factor/caja)*caja;
+  let hei = int(windowHeight*factor/caja)*caja;
+  createCanvas(wid, hei);
 };
 function draw(){
   log("Why not?");
