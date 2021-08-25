925cc620d2af47193640565bd5e523f06579db21
JoanIgnasiMarti <44504641+JoanIgnasiMarti@users.noreply.github.com>
2021-08-25 10:10:44 +0200

    Update convo.js

--- p5js_code/convo.js
+++ p5js_code/convo.js
@@ -1,7 +1,10 @@
+log("testing");
 function setup(){
+  log("should work");
   createCanvas(windowWidth, windowHeight);
 };
 function draw(){
+  log("Why not?");
   background(0);
   fill(255);
   ellipse(mouseX, mouseY,10,10);
